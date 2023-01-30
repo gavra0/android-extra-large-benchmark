ba5f187c3af4ef9e1efeabe8fa00ba87fefc2c16
Ahmet Kerim Senol <akerim@google.com>
2023-01-30 11:55:01 +0000

    Shell script for setting up project for different module counts
    Also added a settings file for entire project to make it possible to reset the project
    
    Usage:
    `./apply_diff.sh 500`
    To reset:
    `./apply_diff.sh all`

--- apply_diff.sh
+++ apply_diff.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 # usage `./apply_diff.sh 500`
 MODULE_COUNT=$1
+# 50 and 100 intentionally left out so it removes the max heap argument
+# and falls back to default.
 declare -A MAX_MEMORY=(
-  ["50"]="400m"
-  ["100"]="600m"
-  ["200"]="1300m"
-  ["500"]="4000m"
-  ["1000"]="9000m"
-  ["app"]="20000m"
-  ["all"]="60g"
+  ["200"]="-Xmx1100m"
+  ["500"]="-Xmx3200m"
+  ["1000"]="-Xmx6800m"
+  ["app"]="-Xmx20000m"
+  ["all"]="-Xmx60g"
 )
 
 cp "settings-$MODULE_COUNT.gradle" "settings.gradle"
-sed -i "s/-Xmx\w*/-Xmx${MAX_MEMORY[${MODULE_COUNT}]}/" "gradle.properties"
+sed -i "s/-Xmx\w*/${MAX_MEMORY[${MODULE_COUNT}]}/" "gradle.properties"
 #TODO: Adjust the IDE memory in the sync scenario file too
